fae4d06cb53eb599f7275da747fa6080d1a685da
Tim van Dam <timvandamcs@gmail.com>
2020-03-27 21:32:17 +0100

    Client instances try to reconnect automatically

--- example/client.js
+++ example/client.js
@@ -3,6 +3,12 @@ const { Client } = require('../index')
 const client = new Client('localhost', 1250)
 const { logMessage } = client.methods
 
+// Handle (fatal) errors
+client.on('error', error => {
+  console.log(`Client error - ${error.message}`)
+  process.exit(0)
+})
+
 process.stdin.setEncoding('utf8')
 process.stdin.on('readable', () => {
   let chunk
@@ -10,6 +16,5 @@ process.stdin.on('readable', () => {
   while ((chunk = process.stdin.read()) !== null) {
     logMessage(chunk.trim(), Date.now())
       .catch(error => console.log(`Could not send message - ${error}`))
-    process.exit(0)
   }
 })
