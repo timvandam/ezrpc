5dcbb577feb48eed25dc3bbc0beb13ce88fc5d1b
Tim van Dam <timvandamcs@gmail.com>
2020-03-28 17:31:49 +0100

    changed server example slightly

--- example/server.js
+++ example/server.js
@@ -2,8 +2,13 @@ const { Server } = require('../index')
 
 const server = new Server(1250)
 
-server.addMethods(
-  function logMessage (msg, time) {
+server.module.exports = {
+  logMessage: (msg, time) => {
     console.log(msg, Date.now() - time, 'ms')
   }
-)
+}
+/* This is equivalent to:
+  server.addMethods(function logMessage () { ... })
+  server.addMethods({ logMessage: () => { ... } })
+  server.methods.logMessage = () => { ... }
+*/
