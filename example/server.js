d53a8494daedee8dfbda9aac7cfbe7364389112b
Tim van Dam <timvandamcs@gmail.com>
2020-03-28 18:14:55 +0100

    changed server example

--- example/server.js
+++ example/server.js
@@ -2,8 +2,10 @@ const { Server } = require('../index')
 
 const server = new Server(1250)
 
+function logMessage (msg, time) {
+  console.log(msg, Date.now() - time, 'ms')
+}
+
 server.module.exports = {
-  logMessage: (msg, time) => {
-    console.log(msg, Date.now() - time, 'ms')
-  }
+  logMessage
 }
