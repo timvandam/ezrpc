b6518d473ae6d2a5a97fe79e82cf675480858947
Tim van Dam <timvandamcs@gmail.com>
2020-03-29 19:11:48 +0200

    loadbalancer example client now handles fatal errors

--- examples/loadbalancer/client.js
+++ examples/loadbalancer/client.js
@@ -3,6 +3,12 @@ const { Client } = require('../../')
 // Connect to a server and assign the myMethod variable
 const { myMethod } = new Client('localhost', 1250).methods
 
+// Handle fatal errors
+client.on('error', error => {
+  console.log(`Client error - ${error.message}`)
+  process.exit(0)
+})
+
 // Call myMethod, log whatever happens, then exit
 myMethod()
   .then(value => console.log(`myMethod was succesfully called and returned ${value}`))
