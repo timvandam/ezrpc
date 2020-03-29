ac62db45b2540906abc04b448fe06189c9a8b012
Tim van Dam <timvandamcs@gmail.com>
2020-03-29 18:30:31 +0200

    removed loadbalancer example; will create one that makes more sense

--- example/loadBalancedExample/loadbalancer.js
+++
@@ -1,4 +0,0 @@
-const { LoadBalancer } = require('../../')
-
-// This loadbalancer functions like a reverse proxy to a server running on port 1251
-const server = new LoadBalancer([{ host: 'localhost', port: 1251 }], 1250)
